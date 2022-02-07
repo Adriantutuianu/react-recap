bf7452628f9ffc7678fb3ac489932281bbeafa07
Adrian <adriantutuianu01@gmail.com>
2022-02-07 19:25:25 +0000

    Saved number as state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import "./App.css";
 function App() {
   const [name, setName] = useState("Adrian");
   const [isActive, setIsActive] = useState(false);
-  console.log(name);
+  const [age, setAge] = useState(33);
+
   return (
     <div className="App">
       <p style={{ color: isActive ? "red" : "green" }}>{name}</p>
+
       <button onClick={() => setName("Johnny")}>Change name to Johhny </button>
+
       <button onClick={() => setName("Adrian")}>
         Change name back to Adrian{" "}
       </button>
+
       <button onClick={() => setIsActive(!isActive)}>Toggle active</button>
+
+      <p>Adrian is {age} years old.</p>
+      <p>Next year Adrian will be {age + 1}.</p>
     </div>
   );
 }
