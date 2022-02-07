f88fc89dd5cc383f225b0c2382a1033bd952950f
Adrian <adriantutuianu01@gmail.com>
2022-02-07 19:19:11 +0000

    Conditional rendering- ternary

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("Adrian");
+  const [isActive, setIsActive] = useState(false);
   console.log(name);
   return (
     <div className="App">
-      <p>{name}</p>
+      <p style={{ color: isActive ? "red" : "green" }}>{name}</p>
       <button onClick={() => setName("Johnny")}>Change name to Johhny </button>
       <button onClick={() => setName("Adrian")}>
         Change name back to Adrian{" "}
